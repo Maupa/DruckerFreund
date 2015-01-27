94fe5493dd56dbebca974cbedcfae5376c5a2be8
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2015-01-28 10:57:38 +1300

    Updated the app. Added drivers. Styling.

--- DrukenFreund.app/Contents/MacOS/app/js/app.js
+++ DrukenFreund.app/Contents/MacOS/app/js/app.js
@@ -2,29 +2,35 @@
 
 	$.fn.shopaholic = function(params) {
 		var options = $.extend({
-			data: '/data/items.json' //This line adding another parameter to the options... ```expands``` it
+			data: 'data/printers.json', //This line adding another parameter to the options... ```expands``` it
+			drivers: 'data/drivers.json' //This line adding another parameter to the options... ```expands``` it
 		}, params);
 		var printers = {};
+		var driver = {};
 		var Super = this;
-		var domain = "";
+		var domain = ""; //domain with tp
 		var model = {
 			printer: function(data){
-				return "lpadmin -p " + data.name + " -L \"" + data.location + "\" -E -v "+ domain + data.name + " -P /Library/Printers/PPDs/Contents/Resources/" + data.driver
+				return "lpadmin -p " + data.name + " -L \"" + data.location + "\" -E -v "+ domain + data.name + " -P \"/Library/Printers/PPDs/Contents/Resources/" + driver[data.driver] + "\""
 			}
 		};
 
-		$.getJSON( 'data/items.json', function( data ) {
+		$.getJSON( options.data, function( data ) {
 			$.each( data, function( n, item ) {
 				printers[item.name] = item;
 				var chkbx = $('<p><input type="checkbox" data-name="' + item.name + '" name="checkbox"/>' + item.name + '</p>');
 				Super.prepend(chkbx);
 			});
 		});
+		$.getJSON( options.drivers, function( data ) {
+			driver = data;
+		});
 		options.thatbooton.click(function(){
-			options.shell.html('')
+			options.shell.html('');
 			$('.chk_box input:checked').each(function() {
 				options.shell.append('<p>' + model.printer(printers[$(this).data('name')]) + '</p>');
 			});
+			window.location.href = '#shell';
         });
 	};
 	
