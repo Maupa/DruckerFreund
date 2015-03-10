f56e8bd7e79cd8915a15dd2a636b4e67e63e8615
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2015-03-10 21:47:00 +1300

    Moved to ZURB template.

--- js/app.js
+++ js/app.js
@@ -1,86 +1,43 @@
-(function($) {
-
-	$.fn.druckerfreund = function(params) {
-		var options = $.extend({
-			data: 'data/printers.json',
-			drivers: 'data/drivers.json'
-		}, params);
-		var printers = {};
-		var driver = {};
-		var Super = this;
-		var def = '';
-		var model = {
-			printer: function(data){
-				var o = def === data.qname ? ' -d ' : '';
-				var name = data.name === '' ? data.qname : data.name; 
-				return '/usr/sbin/lpadmin -p ' + data.qname + o + '-L "' 
-					+ data.location + '" -E -v ' + options.domain + data.name 
-					+ ' -P "/Library/Printers/PPDs/Contents/Resources/' + driver[data.driver] + '"'
-					+ ' -D "' +  name + '"';
-			}
-		};
-		
-		// Handlers
-		options.thatbooton.click(function(){
-			options.shell.html('');
-			$('option:selected', $(Super)).each(function() {
-				options.shell.append('<p>' + model.printer(printers[$(this).attr('value')]) + '</p>');
-			});
-			window.location.href = '#shell';
+var handlers = {
+	destroy: function(){
+		$(this)
+		.parents().eq(2)
+		.animate({
+			height: 0, 
+			width: 0,
+			opacity: 0
+		}, 250, function(){
+			$(this).remove();
 		});
+	}
+};
+var models = {
+	keyval: function(key, value){
+		return  '<div class="large-12 columns">' +
+					'<div class="large-6 columns tr">"<span class="green">' + key + '</span>" :</div>' +
+					'<div class="large-6 columns tl crop">"<span class="green">' + value + '</span>"</div>' +
+				'</div>';
+	},
+	item: function(type, data){
+		var item = '<div class="' + type + '">' +
+						'<div class="large-12 columns">' +
+							'<div class="large-2 columns top-bracket">{</div>' +
+							'<div class="large-10 columns tr"><button class="close" title="Delete">✖</button></div>' +
+						'</div>';
+						for(key of Object.keys(data)){
+							item += this.keyval(key, data[key]);
+						}
+		item += 		'<div class="large-12 columns bottom-bracket">},</div>' + 
+					'</div>';
+		return item;
 
-		$(Super).change(function(){
-			if($('option:selected', Super).length > 0){
-				$(options.def_printer).html('<option></option>')
-				$('option:selected', Super).each(function() {
-					 $(options.def_printer).append('<option value=\"' + $(this).attr('value') + '\">' + $(this).attr('value') + '</option>');
-				})
-				$(options.def_printer).prop('disabled', false);
-			}else{
-				$(options.def_printer).html('<option></option>')
-				$(options.def_printer).prop('disabled', true);
-				def = '';
-			}
-		});
+	}
 
-		$(options.def_printer).change(function(){
-			$('option:selected', options.def_printer).each(function() {
-				def = $(this).attr('value');
-			})
-		})
+}	
 
-		// loadAndDo function that process multiple async json requests
-		var loadAndDo = function(todo){
-			for(lad of todo){
-				var toLoad = lad[0];
-				var callback = lad[1];
-				$.getJSON(toLoad, callback).fail(function(error){
-					console.log("Error while loading " + toLoad);
-				});
-			}
-		}
-
-		loadAndDo([
-			// Driver load function
-			[options.drivers,  function( data ) {
-					driver = data;
-			}],
-
-			// Printer load function
-			[options.data, function( data ) {
-				$.each( data, function( n, item ) {
-					//Get add printer to the printer list 
-					printers[item.qname] = item;
-
-					var option = $('<option value=\"' + item.qname + '\">' + item.name + '</option>');
-
-					if($("optgroup[label=\'" + item.group + "\']").html() == null ) $(Super).append('<optgroup label=\"' + item.group + '\"></optgroup>');
-
-					$("optgroup[label=\'" + item.group + "\']").append(option);
-				})
-			}]
-		])
-
-	};
-	
-}(jQuery));
+$.getJSON('data/printers.json').done(function(data){
+	for(printer of data){
+		$('.printers').append(models.item('printer', printer));
+	}
+	$('.close').on("click", handlers.destroy);
+})
