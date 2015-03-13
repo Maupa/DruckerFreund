efa08737eb251a19ebcca2e25bfea2c93c23de0b
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2015-03-13 21:42:04 +1300

    Added checker for the input fields.

--- js/df.edit.jquery.js
+++ js/df.edit.jquery.js
@@ -1,12 +1,11 @@
 (function($){
 
 	$.fn.df_edit = function(options){
+		var defs = $.extend({}, options);
 		var structures = {
 			printers: {},
 			drivers: {}
 		};
-
-		var defs = $.extend({}, options);
 
 		var models = {
 			view: {
@@ -32,24 +31,36 @@
 				}
 			},
 			edit: {
-				item: function(type, data){
-				// 	var item = '<div class="printer add">';
-				// 	for(key of Object.keys(data)){
-				// 		item += this.keyval(key, data[key]);
-				// 	}
-				// 		<input type="text" autocomplete="off" name="name"  placeholder="Name"><br/>
-				// 		<input type="text" autocomplete="off" name="qname"  placeholder="Queue name"><br/>
-				// 		<input type="text" autocomplete="off" name="location"  placeholder="Location"><br/>
-				// 		<input type="text" autocomplete="off" name="group"  placeholder="Group"><br/>
-				// 		<input type="text" autocomplete="off" name="driver"  placeholder="Driver"><br/>
-				// 		<input type="button" class="submit" autocomplete="off" name="submit" value="Submit"><br/>
-				// 	</div>
-				}
-
+				item: function(type, data){}
 			}
 
 		};
 
+
+		String.prototype.df_check = function(type, item) {
+			var check = {
+				qname: function(str){
+					return str.search(/[A-Za-z0-9\-\_]+/) !== -1 ? true : false  
+				},
+				driver: function(driver){
+					//Checks if driver exists
+					return structures[driver] !== undefined ? true : false
+				},
+				other: function(str){
+					return str.search(/[\w\d]+/) !== -1 ? true : false  
+				}
+			}
+			
+			if(type !== 'qname' && type !== 'driver'){
+				return check['other'](item);
+			}else{
+				return check[type](item);
+			}
+		};
+
+	
+
+		
 		var handlers = {
 			destroy: function(){
 				$(this)
@@ -87,18 +98,12 @@
 						});
 					}
 				})
-				// $(this)
-				// .parents().eq(2).animate({
-				// 	height: 0, 
-				// 	width: 0,
-				// 	opacity: 0
-				// }, 250);
 			}
 		};
 
 
 		$('.df_btn_add').on('click', handlers.submit);
-		
+
 		var converter = { name : 'Name', qname : 'Queue name', location : 'Location', group : 'Group', driver : 'Driver' };
 		
 		
